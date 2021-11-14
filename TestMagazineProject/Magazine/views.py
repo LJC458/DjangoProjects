e69b2d76df17555036fb6fcfd12d7c654daab70b
Liam Cochrane <liam.cochrane@hotmail.co.uk>
2021-11-14 21:12:26 +0000

    added test views

--- TestMagazineProject/Magazine/views.py
+++ TestMagazineProject/Magazine/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse, response
 from django.shortcuts import render
+from Magazine.models import Posts
 
 # Create your views here.
+def view(request,*args,**kwargs):
+    return HttpResponse('<h1> Hello World </h1>')
+
+def PostList(request,*arg,**kwargs):
+    QSet = Posts.objects.all()
+    return HttpResponse(Qset)
+    
--- TestMagazineProject/TestMagazineProject/urls.py
+++ TestMagazineProject/TestMagazineProject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Magazine import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/',views.view),
+    path('posts/',views.PostList)
 ]
